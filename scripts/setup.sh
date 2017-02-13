b5932e8f0eea0b0889f379832a7c248a22d5f0fd
pedrobarco <pedro.barco@tecnico.ulisboa.pt>
2017-02-13 16:34:57 +0000

    setup script works wherever you put it

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
+	cd "$(dirname "$0")"
+	cd ..
+	DIR=$PWD
+
 function makeLn {
 
 	cd ~
 	rm -rf scripts colors .Xresources .zshrc .zprofile .nanorc .weechat .config/dunst .config/i3 .config/mpv .config/ranger .i3blocks.conf .mpd .mpdscribble .ncmpcpp .vim .vimrc .vimperator .vimperatorrc .compton.conf .xinitrc 
 	mkdir .weechat && mkdir .vim && mkdir .mpd && cd .mpd && mkdir playlists && touch mpd.db mpd.log mpd.pid mpdstate && cd ~
 
-	ln -s ~/Dropbox/Dotfiles/scripts ~/
-	ln -s ~/Dropbox/Dotfiles/colors ~/
-	ln -s ~/Dropbox/Dotfiles/.weechat/weechat.conf ~/.weechat/
-	ln -s ~/Dropbox/Dotfiles/.weechat/irc.conf ~/.weechat/
-	ln -s ~/Dropbox/Dotfiles/.weechat/buffers.conf ~/.weechat/
-	ln -s ~/Dropbox/Dotfiles/.config/i3 ~/.config/
-	ln -s ~/Dropbox/Dotfiles/.config/dunst ~/.config/
-	ln -s ~/Dropbox/Dotfiles/.config/ranger ~/.config/
-	ln -s ~/Dropbox/Dotfiles/.config/mpv ~/.config/
-	ln -s ~/Dropbox/Dotfiles/.xinitrc ~/
-	ln -s ~/Dropbox/Dotfiles/.Xresources ~/
-	ln -s ~/Dropbox/Dotfiles/.zprofile ~/
-	ln -s ~/Dropbox/Dotfiles/.zshrc ~/
-	ln -s ~/Dropbox/Dotfiles/.nanorc ~/
-	ln -s ~/Dropbox/Dotfiles/.i3blocks.conf ~/
-	ln -s ~/Dropbox/Dotfiles/.mpd/mpd.conf ~/.mpd/
-	ln -s ~/Dropbox/Dotfiles/.mpdscribble ~/
-	ln -s ~/Dropbox/Dotfiles/.ncmpcpp ~/
-	ln -s ~/Dropbox/Dotfiles/.vim/autoload ~/.vim/
-	ln -s ~/Dropbox/Dotfiles/.vim/colors ~/.vim/
-	ln -s ~/Dropbox/Dotfiles/.vimrc ~/
-	ln -s ~/Dropbox/Dotfiles/.vimperator ~/
-	ln -s ~/Dropbox/Dotfiles/.vimperatorrc ~/
-	ln -s ~/Dropbox/Dotfiles/.compton.conf ~/
+	ln -s $DIR/scripts ~/
+	ln -s $DIR/colors ~/
+	ln -s $DIR/.weechat/weechat.conf ~/.weechat/
+	ln -s $DIR/.weechat/irc.conf ~/.weechat/
+	ln -s $DIR/.weechat/buffers.conf ~/.weechat/
+	ln -s $DIR/.config/i3 ~/.config/
+	ln -s $DIR/.config/dunst ~/.config/
+	ln -s $DIR/.config/ranger ~/.config/
+	ln -s $DIR/.config/mpv ~/.config/
+	ln -s $DIR/.xinitrc ~/
+	ln -s $DIR/.Xresources ~/
+	ln -s $DIR/.zprofile ~/
+	ln -s $DIR/.zshrc ~/
+	ln -s $DIR/.nanorc ~/
+	ln -s $DIR/.i3blocks.conf ~/
+	ln -s $DIR/.mpd/mpd.conf ~/.mpd/
+	ln -s $DIR/.mpdscribble ~/
+	ln -s $DIR/.ncmpcpp ~/
+	ln -s $DIR/.vim/autoload ~/.vim/
+	ln -s $DIR/.vim/colors ~/.vim/
+	ln -s $DIR/.vimrc ~/
+	ln -s $DIR/.vimperator ~/
+	ln -s $DIR/.vimperatorrc ~/
+	ln -s $DIR/.compton.conf ~/
 }
 
 function changeWp {
 
-	nitrogen --set-zoom-fill --save ~/Dropbox/Dotfiles/Wallpapers/comfy_reading.jpg
+	nitrogen --set-zoom-fill --save $DIR/Wallpapers/comfy_reading.jpg
 }
 
 function installZsh {
@@ -42,23 +46,20 @@ function installZsh {
         cd ~
         git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
         chsh -s /bin/zsh
-        mkdir -p ~/.oh-my-zsh/custom/themes/
-        cd ~/.oh-my-zsh/custom/themes/
-        wget https://raw.github.com/S1cK94/minimal/master/minimal.zsh-theme
-        cd ~
 }
 
 function setupConfig {
 
-        sudo cp scripts/urxvtclip /usr/lib/urxvt/perl/clipboard
-        sudo cp ~/Dropbox/Dotfiles/fonts/*.otf ~/usr/share/fonts/OTF/
-        unzip ~/Dropbox/Dotfiles/fonts/bitmap.zip -d Downloads/ 
-        sudo cp -avr ~/Downloads/bitmap/ /usr/share/fonts
+        sudo cp $DIR/scripts/clipboard /usr/lib/urxvt/perl/
+        sudo cp $DIR/fonts/* ~/usr/share/fonts/
+        mkdir -p ~/.fonts/
+        unzip $DIR/fonts/bitmap.zip -d Downloads/ 
+        sudo cp -avr ~/Downloads/bitmap/ .fonts/
         fc-cache -fv
         rm -rf ~/Downloads/bitmap/
 }
 
 setupConfig
-installZsh
-makeLn
-changeWp
+# installZsh
+# makeLn
+# changeWp
