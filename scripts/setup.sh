977f254a892dd290d208159c76d4a4a3082da716
pedrobarco <pedro.barco@tecnico.ulisboa.pt>
2017-02-27 01:03:45 +0000

    added fish-like completion to zsh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -45,6 +45,7 @@ function installZsh {
 
         cd ~
         git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
         chsh -s /bin/zsh
 }
 
