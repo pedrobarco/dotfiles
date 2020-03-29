b89e89f0da2dd5fb737a1e7b1cf191ad832f8986
Pedro Barco <pedro.barco@linkconsulting.com>
2020-03-29 14:09:42 +0100

    Add antibody plugins and setup script

--- scripts/scripts/hosts.sh
+++ scripts/scripts/hosts.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-sudo cp hosts /etc/hosts
-rm hosts
-cd ~
+# Copy hosts file from StevenBlack
+echo "Updating hosts file to StevenBlack hosts..."
+wget -O /etc/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
