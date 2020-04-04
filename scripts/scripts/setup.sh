856356700d61d4009bc7b226be350a87a792a360
Pedro Barco <pedro.barco@linkconsulting.com>
2020-04-04 15:36:18 +0100

    Add shell setup script and tmux config

--- scripts/scripts/setup.sh
+++ scripts/scripts/setup.sh
@@ -2,10 +2,11 @@
 
 # Install essential packages
 echo "Installing essential packages..."
-apt install -y git vim tmux stow
+apt install -y git vim stow
 
 # Run other scripts
 echo "Running setup scripts..."
-sh ./zsh.sh
+sh ./shell.sh
+sh ./tmux.sh
 sh ./hosts.sh
 sh ./fonts.sh
--- scripts/scripts/shell.sh
+++ scripts/scripts/shell.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Install zsh
 echo "Installing zsh..."
 apt install -y zsh
 
-# Install antibody
 echo "Installing antibody - zsh plugin manager..."
 curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
 
