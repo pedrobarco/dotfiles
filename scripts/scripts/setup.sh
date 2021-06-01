4e9ad46e4ae132c1c5d0d439a3a3abb9a651099a
Pedro Barco <pedro.barco@sky.uk>
2021-06-01 22:24:02 +0100

    add mac setup script

--- scripts/scripts/setup.sh
+++ scripts/scripts/setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Install essential packages
-echo "Installing essential packages..."
-apt install -y git vim stow
+# Install homebrew 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install brew normal packages
+brew install stow vim tmux git git-flow tig zsh tree watch htop wget telnet jq antibody yarn kubectl docker-compose firefox vlc
+
+# Install brew casks packages
+brew install --cask docker vscodium thunderbird tor-browser
+
+# Clone tmux tpm 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Run other scripts
-echo "Running setup scripts..."
 sh ./shell.sh
 sh ./tmux.sh
 sh ./hosts.sh
