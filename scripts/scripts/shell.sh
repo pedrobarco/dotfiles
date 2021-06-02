2adc832f0c1ec0e65a0c1455a448d3f43ec1c055
Pedro Barco <pedro.barco@sky.uk>
2021-06-02 10:44:05 +0100

    refactor setup_mac.sh

--- scripts/scripts/shell.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Installing antibody - zsh plugin manager..."
-curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
-
--- scripts/scripts/tmux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing tmux..."
-apt install -y tmux
-
-echo "Installing tmux plugin manager..."
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
