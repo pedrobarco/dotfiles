687836ced93a8a4fcc1b49896b054988d80f85ec
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2022-06-06 03:09:32 +0100

    feat(nvim): add fzy telescope plugin

--- nvim/.config/nvim/lua/usrbinboat/telescope.lua
+++ nvim/.config/nvim/lua/usrbinboat/telescope.lua
@@ -10,5 +10,6 @@ require("telescope").setup({
         }
     }
 })
+require("telescope").load_extension("fzy_native")
 require("telescope").load_extension("file_browser")
 require("telescope").load_extension("project")
