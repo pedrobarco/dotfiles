d1ec15b7424f41fddcb376a747265451e0635db4
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2022-06-12 00:53:19 +0100

    feat(nvim): add markdown-preview plugin

--- nvim/.config/nvim/lua/usrbinboat/telescope.lua
+++ nvim/.config/nvim/lua/usrbinboat/telescope.lua
@@ -1,15 +1,2 @@
-require("telescope").setup({
-    extensions = {
-        project = {
-            base_dirs = {
-                {
-                    vim.env.REPOS,
-                    max_depth = 3
-                }
-            }
-        }
-    }
-})
+require("telescope").setup({})
 require("telescope").load_extension("fzy_native")
-require("telescope").load_extension("file_browser")
-require("telescope").load_extension("project")
