f052f4fad1190fe5b7bed56dd4788772b9687004
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2023-04-19 22:40:40 +0100

    feat: use gruvbox material

--- nvim/.config/nvim/lua/usrbinboat/plugins/lualine.lua
+++ nvim/.config/nvim/lua/usrbinboat/plugins/lualine.lua
@@ -5,30 +5,9 @@ end
 
 lualine.setup({
 	options = {
-		icons_enabled = false,
+		icons_enabled = true,
 		theme = "auto",
 		section_separators = "",
 		component_separators = "",
-	},
-	sections = {
-		lualine_a = { {
-			"mode",
-			fmt = function(str)
-				return str:sub(1, 1)
-			end,
-		} },
-		lualine_b = { "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "filetype" },
-		lualine_y = {},
-		lualine_z = { "location" },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
 	},
 })
