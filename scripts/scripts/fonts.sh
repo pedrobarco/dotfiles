5b41b06a750b1cef2198981f60cb3d54e562102b
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2023-05-15 19:58:38 +0100

    refactor: upgrade nerd fonts

--- scripts/scripts/fonts.sh
+++ scripts/scripts/fonts.sh
@@ -9,7 +9,7 @@ fonts=(
 fdir="$HOME/.fonts"
 for font in "${fonts[@]}"
 do
-    curl -LO "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/$font.zip"
+    curl -LO "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/$font.zip"
     unzip "$font.zip" -d "$fdir/$font"
     rm -rf "$font.zip"
 done
