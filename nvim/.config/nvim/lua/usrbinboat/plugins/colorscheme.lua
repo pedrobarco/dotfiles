3fe084ceb404a148c6fcd3629fb10b0db2b7537a
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2024-01-15 23:53:43 +0000

    feat: enable tundra plugins and dim windows

--- nvim/.config/nvim/lua/usrbinboat/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/usrbinboat/plugins/colorscheme.lua
@@ -11,8 +11,25 @@ return {
 		"sam4llis/nvim-tundra",
 		priority = 1000,
 		config = function()
-			require("nvim-tundra").setup()
+			require("nvim-tundra").setup({
+				dim_inactive_windows = {
+					enabled = true,
+					color = require("nvim-tundra.palette.arctic").gray._875,
+				},
+				plugins = {
+					lsp = true,
+					semantic_tokens = true,
+					treesitter = true,
+					nvimtree = true,
+					cmp = true,
+					context = true,
+					dbui = true,
+					gitsigns = true,
+					neogit = true,
+				},
+			})
 			vim.cmd.colorscheme("tundra")
+			vim.opt.statusline = "%2{mode()} | %{expand('%:p:h:t')}/%t %m %r %= %y %8(%l,%c%) %4p%%"
 		end,
 	},
 }
