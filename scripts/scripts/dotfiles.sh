694005ee7d5e92efd8ddd23a56a68ac7d9205732
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2024-03-23 22:38:49 +0000

    fix: remove vim from stowed folders

--- scripts/scripts/dotfiles.sh
+++ scripts/scripts/dotfiles.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# TODO: clone git repo
-# TODO: detect platform and run setup script
-
 stow --restow -t $HOME shell
-stow --restow -t $HOME vim
 stow --restow -t $HOME nvim
 stow --restow -t $HOME tmux
 stow --restow -t $HOME bin
