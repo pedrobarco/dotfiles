094f2f36ca01d828ebcb8029d259dfb5cd8a4e30
Pedro Barco <pedro.barco@sky.uk>
2024-06-27 09:39:38 +0100

    feat: add keymap to yank to system clipboard

--- nvim/.config/nvim/lua/usrbinboat/core/keymaps.lua
+++ nvim/.config/nvim/lua/usrbinboat/core/keymaps.lua
@@ -1,6 +1,8 @@
 -- set leader key to space
 vim.g.mapleader = " "
 
+vim.keymap.set("v", "<leader>y", '"*y') -- copy to system clipboard
+
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move selection up
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move selection down
 
