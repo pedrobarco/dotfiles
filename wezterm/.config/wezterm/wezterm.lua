4dadd4fa7c385254756894f2ddf90ad9100969b6
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2025-01-24 19:35:42 +0000

    feat: disable wezterm window_close_confirmation

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,11 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
 
 local function is_windows()
 	return wezterm.target_triple == "x86_64-pc-windows-msvc"
 end
 
-config.color_scheme = "gruvbox_material"
-config.font = wezterm.font({
-	family = "JetBrainsMono Nerd Font",
-	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-})
-config.font_size = 16
+local config = wezterm.config_builder()
+-- window settings
 config.enable_tab_bar = false
 config.window_padding = {
 	left = 8,
@@ -18,6 +13,17 @@ config.window_padding = {
 	top = 8,
 	bottom = 8,
 }
+config.window_close_confirmation = "NeverPrompt"
+
+-- font settings
+config.font = wezterm.font({
+	family = "JetBrainsMono Nerd Font",
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+})
+config.font_size = 16
+
+-- color settings
+config.color_scheme = "gruvbox_material"
 config.color_schemes = {
 	["gruvbox_material"] = require("themes/gruvbox_material"),
 }
