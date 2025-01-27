1991f52cf84707d98eaec0ca266aa006dbf15707
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2025-01-27 19:16:01 +0000

    feat: set diffview merge layout to diff3_mixed

--- nvim/.config/nvim/lua/usrbinboat/plugins/neogit.lua
+++ nvim/.config/nvim/lua/usrbinboat/plugins/neogit.lua
@@ -3,7 +3,42 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope.nvim",
-		"sindrets/diffview.nvim",
+		{
+			"sindrets/diffview.nvim",
+			opts = {
+				enhanced_diff_hl = true,
+				view = {
+					merge_tool = {
+						layout = "diff3_mixed",
+					},
+				},
+				file_panel = {
+					listing_style = "list",
+					win_config = {
+						position = "bottom",
+						height = 10,
+						win_opts = {},
+					},
+				},
+				key_bindings = {
+					file_history_panel = {
+						q = function()
+							require("diffview").close()
+						end,
+					},
+					file_panel = {
+						q = function()
+							require("diffview").close()
+						end,
+					},
+					view = {
+						q = function()
+							require("diffview").close()
+						end,
+					},
+				},
+			},
+		},
 	},
 	opts = {
 		disable_commit_confirmation = true,
