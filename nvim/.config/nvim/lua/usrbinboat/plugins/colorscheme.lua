77b16f31bafc21f8f4551487cd6a03205ba94550
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2025-02-02 18:55:25 +0000

    refactor: remove tundra from neovim colorschemes

--- nvim/.config/nvim/lua/usrbinboat/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/usrbinboat/plugins/colorscheme.lua
@@ -2,19 +2,10 @@ return {
 	{
 		"sainnhe/gruvbox-material",
 		priority = 1000,
+		enabled = true,
 		config = function()
 			vim.g.gruvbox_material_background = "hard"
 			vim.cmd.colorscheme("gruvbox-material")
-		end,
-	},
-	{
-		"sam4llis/nvim-tundra",
-		priority = 1000,
-		enabled = false,
-		config = function()
-			vim.g.tundra_biome = "jungle" -- 'arctic' or 'jungle'
-			vim.opt.background = "dark"
-			vim.cmd.colorscheme("tundra")
 		end,
 	},
 }
