e81873546527c09e9e22f205cb17f2eb66efdd80
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2025-07-17 11:02:33 +0100

    feat: lazy load lazydev luvit types

--- nvim/.config/nvim/lua/usrbinboat/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/usrbinboat/plugins/lsp/lspconfig.lua
@@ -6,7 +6,12 @@ return {
 			{
 				"folke/lazydev.nvim",
 				ft = "lua",
-				opts = {},
+				opts = {
+					library = {
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
 			},
 			{ "hrsh7th/cmp-nvim-lsp" },
 		},
